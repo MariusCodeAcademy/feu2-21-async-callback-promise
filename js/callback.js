46b613fecc90a4ab1ccdce171697e4f05dd47456
Marius <marius@example.com>
2022-11-09 08:56:12 +0200

    time, post problem

--- js/callback.js
+++ js/callback.js
@@ -1,33 +1,22 @@
 'use strict';
 console.log('callback.js');
 
-const btn1El = document.getElementById('btn1');
-const timeEl = document.getElementById('time');
-
-btn1El.addEventListener('click', () => {
-  console.log('Paspaudei');
-  // ivygdo nurodyta funkcija po nurodyto laiko
+function getPost1() {
   setTimeout(() => {
-    console.log('Paspaudei timeout');
+    console.log('post1');
+  }, 3000);
+}
+function getPost2() {
+  setTimeout(() => {
+    console.log('post2');
   }, 2000);
-});
-
-function sayHi() {
-  console.log('Hello timeOut');
+}
+function getPost3() {
+  setTimeout(() => {
+    console.log('post3');
+  }, 1800);
 }
 
-// ivygdo nurodyta funkcija po nurodyto laiko
-setTimeout(sayHi, 3000);
-
-// setInterval vygdo kazkoki koda kas kazkiek ms
-const int1 = setInterval(tick, 1000);
-
-function tick() {
-  const now = new Date();
-  // timeEl.textContent = now.toLocaleTimeString();
-  timeEl.textContent = now.toLocaleString('lt-LT', { timeStyle: 'medium' });
-}
-// tick();
-
-// sutabdyti intervala su clearInterval
-setTimeout(() => clearInterval(int1), 5000);
+getPost1();
+getPost2();
+getPost3();
