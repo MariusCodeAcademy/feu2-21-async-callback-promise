43fe65e786186b557108f0705619f5b8ae3bba42
Marius <marius@example.com>
2022-11-09 10:34:45 +0200

    promisifying getPosts

--- js/callback.js
+++ js/callback.js
@@ -1,36 +1,39 @@
 'use strict';
 console.log('callback.js');
 
-function getPost1(callBackFunc) {
-  setTimeout(() => {
-    console.log('post1');
-    // cia as noriu kviesti post2
-    callBackFunc();
-  }, 3000);
+function getPost1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('post1');
+      // cia as noriu kviesti post2
+      resolve();
+    }, 3000);
+  });
 }
 function getPost2() {
-  setTimeout(() => {
-    console.log('post2');
-  }, 2000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('post2');
+
+      resolve();
+    }, 2000);
+  });
 }
+
 function getPost3() {
+  // throw new Error('klaida rasant'); // bus pagauta catch
   setTimeout(() => {
     console.log('post3');
   }, 1800);
 }
 
-getPost1(getPost2);
+// callback way
+// getPost1(getPost2);
 // getPost2();
 // getPost3();
 
-// kada call back gali buti naudojamas
-function makeNum(cb) {
-  const rez = cb(5);
-  return rez;
-}
-
-function double(num) {
-  return num * 2;
-}
-const n1 = makeNum(double);
-// double(n1);
+// promise Way
+getPost1()
+  .then(() => getPost2())
+  .then(() => getPost3())
+  .catch((err) => console.warn('klaida kazkur get post', err));
