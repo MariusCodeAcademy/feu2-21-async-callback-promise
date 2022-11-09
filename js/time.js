3dd6e4d03a3754352a3ba3d614a88554ddfaae0c
Marius <marius@example.com>
2022-11-09 10:10:00 +0200

    promise example

--- js/time.js
+++ js/time.js
@@ -1,7 +1,7 @@
 const timeEl = document.getElementById('time');
 
 const int1 = setInterval(tick, 1000);
-
+tick();
 function tick() {
   const now = new Date();
   // timeEl.textContent = now.toLocaleTimeString();
